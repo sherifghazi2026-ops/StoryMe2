112c8cf337006aa043d367d548ac9047f8e85cb4
sherifghazi2026-ops <sherifghazi2026@gmail.com>
2026-05-07 11:15:43 +0300

    Delete data directory

--- data/index.js
+++
@@ -1,8 +0,0 @@
-import Pack1 from '../packages/pack1';
-import Pack2 from '../packages/pack2';
-import Pack3 from '../packages/pack3';
-import Pack4 from '../packages/pack4';
-import Pack5 from '../packages/pack5';
-
-const AllPacks = { pack1: Pack1, pack2: Pack2, pack3: Pack3, pack4: Pack4, pack5: Pack5 };
-export default AllPacks;
